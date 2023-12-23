1c066109500d4342fdc62b115e2511751de886ae
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-23 15:59:50 +0530

    register view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from serializers import UserRegister
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 # Create your views here.
+
+class Register(APIView):
+    def post(self,request,formate=None):
+        serializer=UserRegister(data=request.data)
+        data={}
+        if serializer.is_valid():
+            account=serializer.save()
+            data['response']='registered'
+            data['username']='account.username'
+            data['email']=account.email
+            token=Token.objects.get(user=account).key
--- restapi/urls.py
+++ restapi/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register',views.Register.as_view,name='register')
 ]
